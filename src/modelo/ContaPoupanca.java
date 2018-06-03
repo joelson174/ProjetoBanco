e0504d42e42058a7407d37eaf7048e39d3333621
Joelson Lima <joelson174@users.noreply.github.com>
2018-06-03 14:51:32 -0400

    Update ContaPoupanca.java

--- src/modelo/ContaPoupanca.java
+++ src/modelo/ContaPoupanca.java
@@ -3,20 +3,17 @@ package modelo;
 public class ContaPoupanca extends Conta {
 	private String dataAniver;
 	private double jurosRend;
-	private boolean ativo;
 
 	public ContaPoupanca() {
 		super();
 		this.setDataAniver("");
 		this.setJurosRend(0);
-		this.setAtivo(true);
 	}
 
 	public ContaPoupanca(String codigo, double saldo, String dataAbert, String dataAniver, double jurosRend) {
 		super(codigo, saldo, dataAbert);
 		this.setDataAniver(dataAniver);
 		this.setJurosRend(jurosRend);
-		this.setAtivo(true);
 	}
 
 	public void efetuarRendimento() {
@@ -40,13 +37,4 @@ public class ContaPoupanca extends Conta {
 	public void setJurosRend(double jurosRend) {
 		this.jurosRend = jurosRend;
 	}
-
-	public boolean isAtivo() {
-		return ativo;
-	}
-
-	public void setAtivo(boolean ativo) {
-		this.ativo = ativo;
-	}
-
 }
